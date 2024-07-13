30af42a45528505dd4b0a1ce462d3e9ff0ab58e9
thekingofspace <37231416+thekingofspace@users.noreply.github.com>
2024-07-13 07:44:32 -0700

    Update Discordify.lua

--- Discordify.lua
+++ Discordify.lua
@@ -1,6 +1,33 @@
 local Discordify = {}
 local webhookData = {}
 
+local kindDictionary = {
+	[1] = function(data, value)
+		data.embed.description = value
+	end,
+	[2] = function(data, value)
+		data.embed.title = value
+	end,
+	[3] = function(data, value)
+		data.message = value
+	end,
+	[4] = function(data, value)
+		data.embed.footer = value
+	end,
+	[5] = function(data, value)
+		data.embed.image = value
+	end,
+	[6] = function(data, value)
+		data.embed.url = value
+	end,
+	[7] = function(data, value)
+		data.embed.color = value
+	end,
+	[8] = function(data, value)
+		data.embed.timestamp = value
+	end
+}
+
 function Discordify.init(webhookUrl, channel)
 	webhookData[channel] = {
 		webhookUrl = webhookUrl,
@@ -11,7 +38,8 @@ function Discordify.init(webhookUrl, channel)
 			footer = nil,
 			color = nil,
 			image = nil,
-			url = nil
+			url = nil,
+			timestamp = nil
 		},
 		message = nil,
 		fieldsDictionary = {}
@@ -25,23 +53,28 @@ function Discordify.Add(kind, value, channel)
 	end
 
 	local data = webhookData[channel]
+	local handler = kindDictionary[kind]
 
-	if kind == "description" or kind == "title" or kind == "message" then
-		data[kind] = value
-	elseif kind == "footer" or kind == "image" or kind == "url" then
-		data.embed[kind] = value
-	elseif kind == "color" then
-		data.embed.color = value
-	elseif kind == "addField" then
-		local fieldId = value[1]
-		data.fieldsDictionary[fieldId] = {
-			name = value[2],
-			value = value[3],
-			inline = value[4]
-		}
+	if handler then
+		handler(data, value)
 	else
 		error("Invalid kind value.")
 	end
+end
+
+function Discordify.AddField(fieldId, name, value, inline, channel)
+	if not webhookData[channel] then
+		error("Webhook channel '" .. channel .. "' not initialized.")
+		return
+	end
+
+	local data = webhookData[channel]
+
+	data.fieldsDictionary[fieldId] = {
+		name = name,
+		value = value,
+		inline = inline
+	}
 end
 
 function Discordify.RemoveField(fieldId, channel)
@@ -99,7 +132,8 @@ function Discordify.Clear(channel)
 		footer = nil,
 		color = nil,
 		image = nil,
-		url = nil
+		url = nil,
+		timestamp = nil
 	}
 
 	data.message = nil
