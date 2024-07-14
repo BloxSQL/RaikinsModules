910a9b81c719530971499ed73f8333129b6ea360
thekingofspace <37231416+thekingofspace@users.noreply.github.com>
2024-07-14 11:11:34 -0700

    Update Compiler.lua

--- Compiler.lua
+++ Compiler.lua
@@ -32,6 +32,23 @@ function Compiler.add(dataName, dataInput, compiledMain)
 	mainDataValue.Value = game.HttpService:JSONEncode(combinedData)
 end
 
+function Compiler.addFP(dataName, dataInput, compiledMain, parentObject)
+	local combinedData
+
+	local mainDataValue = parentObject:FindFirstChild(compiledMain)
+	if not mainDataValue then
+		mainDataValue = Instance.new("StringValue")
+		mainDataValue.Name = compiledMain
+		mainDataValue.Parent = parentObject
+		combinedData = {}
+	else
+		combinedData = game.HttpService:JSONDecode(mainDataValue.Value)
+	end
+
+	combinedData[dataName] = dataInput
+	mainDataValue.Value = game.HttpService:JSONEncode(combinedData)
+end
+
 function Compiler.decompress(dataName, compiledMain)
 	local folder = getOrCreateCompiledDataFolder()
 	local mainDataValue = folder:FindFirstChild(compiledMain)
@@ -45,22 +62,31 @@ function Compiler.decompress(dataName, compiledMain)
 	return combinedData[dataName]
 end
 
-function Compiler.Transfer(dataToTransfer, compiledMain)
-	local folder = getOrCreateCompiledDataFolder()
-	local mainDataValue = folder:FindFirstChild(compiledMain)
+function Compiler.decompressFP(dataName, compiledMain, parentObject)
+	local mainDataValue = parentObject:FindFirstChild(compiledMain)
 
 	if not mainDataValue then
-		mainDataValue = Instance.new("StringValue")
-		mainDataValue.Name = compiledMain
-		mainDataValue.Parent = folder
+		warn("No compiled data found with the name: " .. compiledMain)
+		return nil
 	end
 
-	mainDataValue.Value = dataToTransfer
+	local combinedData = game.HttpService:JSONDecode(mainDataValue.Value)
+	return combinedData[dataName]
 end
 
 function Compiler.Scrub(compiledMain)
 	local folder = getOrCreateCompiledDataFolder()
 	local mainDataValue = folder:FindFirstChild(compiledMain)
+
+	if mainDataValue then
+		mainDataValue:Destroy()
+	else
+		warn("No compiled data found with the name: " .. compiledMain)
+	end
+end
+
+function Compiler.ScrubFP(compiledMain, parentObject)
+	local mainDataValue = parentObject:FindFirstChild(compiledMain)
 
 	if mainDataValue then
 		mainDataValue:Destroy()
@@ -88,28 +114,22 @@ function Compiler.Erase(dataName, compiledMain)
 	end
 end
 
-function Compiler.Build(compiledMain)
-	local folder = getOrCreateCompiledDataFolder()
-	local mainDataValue = folder:FindFirstChild(compiledMain)
+function Compiler.EraseFP(dataName, compiledMain, parentObject)
+	local mainDataValue = parentObject:FindFirstChild(compiledMain)
 
 	if not mainDataValue then
 		warn("No compiled data found with the name: " .. compiledMain)
-		return nil
-	end
-
-	return mainDataValue.Value
-end
-
-function Compiler.exist(dataName, compiledMain)
-	local folder = getOrCreateCompiledDataFolder()
-	local mainDataValue = folder:FindFirstChild(compiledMain)
-
-	if not mainDataValue then
-		return false
+		return
 	end
 
 	local combinedData = game.HttpService:JSONDecode(mainDataValue.Value)
-	return combinedData[dataName] ~= nil
+
+	if combinedData[dataName] then
+		combinedData[dataName] = nil
+		mainDataValue.Value = game.HttpService:JSONEncode(combinedData)
+	else
+		warn("No data found with the name: " .. dataName .. " in " .. compiledMain)
+	end
 end
 
 return Compiler
